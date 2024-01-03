387f8b470b4d6d2b47a1521a711ad477507b51f9
Don Kackman <dkackman@gmail.com>
2024-01-02 20:18:59 -0600

    more tests

--- src/bls.tests/EllipticCurveTests.cs
+++ src/bls.tests/EllipticCurveTests.cs
@@ -6,9 +6,11 @@ namespace bls.tests;
 public class EllipticCurveTests
 {
     private readonly JacobianPoint g = JacobianPoint.GenerateG1();
+    private readonly JacobianPoint g2 = JacobianPoint.GenerateG2();
+    private readonly BigInteger q = Constants.Q;
 
     [Fact]
-    public void IsOnCurve()
+    public void G1IsOnCurve()
     {
         Assert.True(g.IsOnCurve());
     }
@@ -29,5 +31,19 @@ public class EllipticCurveTests
     public void TripleSameAsAddition()
     {
         Assert.True(g.Multiply(3).Equals(g.Add(g).Add(g)));
+    }
+
+    [Fact]
+    public void IndividualMultiplication()
+    {
+        Assert.True(
+            g2.X.Multiply(new Fq(q, 2).Multiply(g2.Y)).Equals(new Fq(q, 2).Multiply(g2.X.Multiply(g2.Y)))
+        );
+    }
+
+    [Fact]
+    public void G2IsOnCurve()
+    {
+        Assert.True(g2.IsOnCurve());
     }
 }
