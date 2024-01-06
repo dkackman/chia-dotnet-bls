91e18134900d9bd18f424de20cfc2e1a40c6642f
Don Kackman <dkackman@gmail.com>
2024-01-05 18:15:48 -0600

    SWUTests

---
+++ src/bls.tests/XmdTests.cs
@@ -0,0 +1,70 @@
+using System.Security.Cryptography;
+using chia.dotnet.bls;
+
+namespace bls.tests;
+
+public class XMDTests
+{
+    private readonly byte[] msg = RandomNumberGenerator.GetBytes(48);
+    private readonly byte[] dst = RandomNumberGenerator.GetBytes(16);
+    private Dictionary<byte[], int> ress = new(new ByteArrayComparer());
+
+    [Fact]
+    public void LengthsAreCorrect()
+    {
+        for (int length = 16; length < 8192; length++)
+        {
+            var result = HashToFieldClass.ExpandMessageXmd(msg, dst, length, HashInfo.Sha512);
+            Assert.Equal(length, result.Length);
+
+            var key = result.Take(16).ToArray();
+            key = ress.Keys.FirstOrDefault(buffer => ByteUtils.BytesEqual(key, buffer)) ?? key;
+            if (ress.TryGetValue(key, out int value))
+            {
+                ress[key] = ++value;
+            }
+            else
+            {
+                ress.Add(key, 1);
+            }
+        }
+    }
+
+    [Fact]
+    public void AllOnes()
+    {
+        foreach (var item in ress.Values)
+        {
+            Assert.Equal(1, item);
+        }
+    }
+}
+
+public class ByteArrayComparer : IEqualityComparer<byte[]>
+{
+    public bool Equals(byte[]? x, byte[]? y)
+    {
+        if (ReferenceEquals(x, y))
+            return true;
+
+        if (x is null || y is null)
+            return false;
+
+        if (x.Length != y.Length)
+            return false;
+
+        for (int i = 0; i < x.Length; i++)
+        {
+            if (x[i] != y[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int GetHashCode(byte[] obj)
+    {
+        return obj.Sum(b => b);
+    }
+}
--- src/bls/utils/crypto/HashToFieldClass.cs
+++ src/bls/utils/crypto/HashToFieldClass.cs
@@ -8,6 +8,7 @@ public static class HashToFieldClass
     {
         if (value < 0 || value >= BigInteger.One << (8 * length))
             throw new Exception($"Bad I2OSP call: value={value}, length={length}.");
+
         var bytes = new byte[length];
         for (int i = length - 1; i >= 0; i--)
         {
@@ -32,44 +33,49 @@ public static class HashToFieldClass
 
     public static byte[] ExpandMessageXmd(byte[] message, byte[] dst, int length, HashInfo hash)
     {
-        int ell = (length + hash.ByteSize - 1) / hash.ByteSize;
+        var ell = (length + hash.ByteSize - 1) / hash.ByteSize;
         if (ell > 255)
             throw new Exception($"Bad expandMessageXmd call: ell={ell} out of range.");
-        byte[] dst_prime = dst.Concat(I2OSP(dst.Length, 1)).ToArray();
-        byte[] Z_pad = I2OSP(0, hash.BlockSize);
-        byte[] lib_str = I2OSP(length, 2);
-        byte[] b_0 = hash.Convert(Z_pad.Concat(message).Concat(lib_str).Concat(I2OSP(0, 1)).Concat(dst_prime).ToArray());
+
+        byte[] dst_prime = [.. dst, .. I2OSP(dst.Length, 1)];
+        var Z_pad = I2OSP(0, hash.BlockSize);
+        var lib_str = I2OSP(length, 2);
+        var b_0 = hash.Convert(Z_pad.Concat(message).Concat(lib_str).Concat(I2OSP(0, 1)).Concat(dst_prime).ToArray());
+
         List<byte[]> bValues = [hash.Convert([.. b_0, .. I2OSP(1, 1), .. dst_prime])];
         for (int i = 1; i <= ell; i++)
         {
             bValues.Add(hash.Convert(BytesXor(b_0, bValues[i - 1]).Concat(I2OSP(i + 1, 1)).Concat(dst_prime).ToArray()));
         }
+
         List<byte> pseudoRandomBytes = [];
         foreach (var item in bValues)
+        {
             pseudoRandomBytes.AddRange(item);
+        }
 
         return pseudoRandomBytes.Take(length).ToArray();
     }
 
     public static byte[] ExpandMessageXof(byte[] message, byte[] dst, int length, HashInfo hash)
     {
-        byte[] dst_prime = dst.Concat(I2OSP(dst.Length, 1)).ToArray();
-        byte[] message_prime = message.Concat(I2OSP(length, 2)).Concat(dst_prime).ToArray();
+        byte[] dst_prime = [.. dst, .. I2OSP(dst.Length, 1)];
+        byte[] message_prime = [.. message, .. I2OSP(length, 2), .. dst_prime];
         return hash.Convert(message_prime).Take(length).ToArray();
     }
 
     public static List<List<BigInteger>> HashToField(byte[] message, int count, byte[] dst, BigInteger modulus, int degree, int byteLength, Func<byte[], byte[], int, HashInfo, byte[]> expand, HashInfo hash)
     {
-        int lengthInBytes = count * degree * byteLength;
-        byte[] pseudoRandomBytes = expand(message, dst, lengthInBytes, hash);
-        List<List<BigInteger>> uValues = new List<List<BigInteger>>();
-        for (int i = 0; i < count; i++)
+        var lengthInBytes = count * degree * byteLength;
+        var pseudoRandomBytes = expand(message, dst, lengthInBytes, hash);
+        List<List<BigInteger>> uValues = [];
+        for (var i = 0; i < count; i++)
         {
-            List<BigInteger> eValues = new List<BigInteger>();
-            for (int j = 0; j < degree; j++)
+            List<BigInteger> eValues = [];
+            for (var j = 0; j < degree; j++)
             {
-                int elmOffset = byteLength * (j + i * degree);
-                byte[] tv = pseudoRandomBytes.Skip(elmOffset).Take(byteLength).ToArray();
+                var elmOffset = byteLength * (j + i * degree);
+                var tv = pseudoRandomBytes.Skip(elmOffset).Take(byteLength).ToArray();
                 eValues.Add(OS2IP(tv) % modulus);
             }
             uValues.Add(eValues);
