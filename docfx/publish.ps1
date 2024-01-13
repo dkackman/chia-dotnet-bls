e67fbc6ca09008814c52920f281315eed0b2e670
Don Kackman <dkackman@gmail.com>
2024-01-13 16:55:47 -0600

    add favicon and logo

--- docfx/publish.ps1
+++
@@ -1,6 +0,0 @@
-$source = ".\\_site"
-$destination = "..\\docs"
-
-docfx build docfx.json
-
-Get-ChildItem -Path $source -Recurse | Copy-Item -Destination { Join-Path $destination $_.FullName.Substring($source.length) } -Force
